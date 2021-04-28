9ee98b6dbf6239e63ff3379fd87da994d0c83425
fashomi emmanuel <fashomi01@gmail.com>
2021-04-28 17:37:24 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head> 
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, debitis similique! Dignissimos, deserunt, soluta architecto quidem assumenda distinctio consequatur deleniti sequi maiores labore nostrum eos autem porro sint quod officia?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, debitis similique! Dignissimos, deserunt, soluta architecto quidem assumenda distinctio consequatur deleniti sequi maiores labore nostrum eos autem porro sint quod officia?</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
